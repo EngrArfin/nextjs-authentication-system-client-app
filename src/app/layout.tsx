b060cd933f605821be6a9732406c9276c7b652f6
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2024-10-06 13:53:01 +0600

    github authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import { Session } from "inspector";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
 
 export const metadata: Metadata = {
   title: "Next Authentication System App",
@@ -13,10 +16,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en" data-theme="light">
       <body>
-        <Navbar />
+        <Navbar session={session} />
         <div className="min-h-screen w-[90%] mx-auto">{children}</div>
       </body>
     </html>
